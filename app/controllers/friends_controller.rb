001f7e9d67d389e6f212e3a6da67d7a286e1d46c
AfifZuhair08 <afifzuhair08@gmail.com>
2021-09-17 14:42:38 +0800

    Added Users Module, by adding db migration, schema, controllers and views, modifying controllers to associate with friends list

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,9 +1,15 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
 
+  # if not auth, just index and show
+  before_action :authenticate_user!
+
+  # check if corret user for the actions
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+
   # GET /friends or /friends.json
   def index
-    @friends = Friend.all
+    @friends = Friend.all.where(user_id: current_user.id)
   end
 
   # GET /friends/1 or /friends/1.json
@@ -12,7 +18,8 @@ class FriendsController < ApplicationController
 
   # GET /friends/new
   def new
-    @friend = Friend.new
+    # @friend = Friend.new
+    @friend = current_user.friends.build
   end
 
   # GET /friends/1/edit
@@ -21,7 +28,8 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    @friend = Friend.new(friend_params)
+    # @friend = Friend.new(friend_params)
+    @friend = current_user.friends.build(friend_params)
 
     respond_to do |format|
       if @friend.save
@@ -56,6 +64,11 @@ class FriendsController < ApplicationController
     end
   end
 
+  def correct_user
+    @friend = current_user.friends.find_by(id: params[:id])
+    redirect_to friends_path, notice: "Not authorize to that action" if @friend.nil?
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_friend
@@ -64,6 +77,6 @@ class FriendsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def friend_params
-      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :address)
+      params.require(:friend).permit(:user_id, :first_name, :last_name, :email, :phone, :address)
     end
 end
